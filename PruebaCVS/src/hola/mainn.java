15351dd2dadc5a733fec9e33bc75ee7bb77a99ab
jose <jose@10.0.2.15>
2014-02-04 23:42:44 +0100

    hola desde 2

--- PruebaCVS/src/hola/mainn.java
+++ PruebaCVS/src/hola/mainn.java
@@ -5,6 +5,10 @@ public class mainn {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
+		
+		
+		System.out.println("hola desde el segundooooo");
 	}
+	
 
 }
