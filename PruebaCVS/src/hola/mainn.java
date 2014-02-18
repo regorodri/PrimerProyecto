60465c9271700e9cc3e685f2fd5a88a310694940
regorodri <regorodri_jl@hotmail.com>
2014-02-18 22:32:28 +0100

    Update mainn.java

--- PruebaCVS/src/hola/mainn.java
+++ PruebaCVS/src/hola/mainn.java
@@ -5,6 +5,8 @@ public class mainn {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
+		System.out.println("Para sincronizarrrrrrrrrrrr diferencias");
+		
 		
 		
 		System.out.println("hola desde el segundooooo");
